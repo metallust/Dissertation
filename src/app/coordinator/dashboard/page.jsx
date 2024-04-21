4d5bcb3a2cc5cfbf27ce0e496bb582b8ca641eed
thelifeisstrange <yogeshpk0123@gmail.com>
2024-04-21 17:22:39 +0530

    coordinator {right and side bar} and login ui

--- src/app/coordinator/dashboard/page.jsx
+++ src/app/coordinator/dashboard/page.jsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Rightbar from "./Rightbar";
+import PgCoordinatorSidebar from "./PgCoordinatorSidebar";
 
 export default function Page() {
 	const router = useRouter();
@@ -134,46 +136,124 @@ export default function Page() {
 		fetchbatches();
 		fetchGuides();
 	}, []);
+
+	const buttonstyle = {
+		background: "none",
+		border: "none",
+	};
+
+	const sidebar = {
+		display: "block",
+		/* background: rgba(0, 0, 0, 0.2), */
+		maxWidth: "280px",
+		flexGrow: "4",
+		overflow: "hidden",
+		overflowY: "scroll",
+	};
+	const main = {
+		display: "flex",
+		height: "100vh",
+	};
+	const content = {
+		display: "block",
+		/* background: rgba(0, 0, 0, 0.4), */
+		flexGrow: "3",
+		overflow: "hidden",
+		overflowY: "scroll",
+	};
+	const rightbar = {
+		background: "rgba(255, 255, 255, 0.2)",
+		minWidth: "140px",
+		maxWidth: "200px",
+		flexGrow: "1",
+		overflow: "hidden",
+		overflowY: "scroll",
+	};
+	const btnstyle = {
+		width: "fit-content",
+		height: "40px",
+		padding: "10px",
+		backgroundColor: "#004257",
+		borderRadius: "10px",
+		border: "none",
+		color: "#fff",
+		fontSize: "16px",
+		fontWeight: "400",
+		boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 4px rgba(0, 0, 0, 0.25)",
+		fontFamily: "Roboto, sans-serif",
+		marginBottom: "20px",
+		marginRight: "10px",
+	};
+
 	return (
-		<div className="min-vh-100 p-4 bg-dark text-danger">
-			Coordinator dashboard
-			<button onClick={logout}>Logout</button>
-			<br />
-			{/* add batch */}
-			<input type="year" placeholder="Year" onChange={(e) => setBatch({ ...batch, year: e.target.value })} value={batch.year} />
-			<input type="text" placeholder="Branch" onChange={(e) => setBatch({ ...batch, branch: e.target.value })} value={batch.branch} />
-			<input type="text" name="domains" placeholder="Domains (comma-separated)" onChange={handleDomainsetChange} value={batch.domainset.join(", ")} />
-			<button onClick={createBatch}>Create Batch</button>
-			<ul>
-				{batches.length !== 0 ? (
-					batches.map((ele) => {
-						return (
-							<li key={ele._id}>
-								year: {ele.year}, branch: {ele.branch}, domainset: {ele.domainset ? ele.domainset.join(", ") : "No domains"}
-								<button onClick={() => router.push("dashboard/" + ele._id)}>GOTO</button>
-							</li>
-						);
-					})
-				) : (
-					<p>no batches</p>
-				)}
-			</ul>
-			{/* add guide */}
-			<br />
-			<input type="text" name="name" placeholder="Name" onChange={(e) => setGuide({ ...guide, name: e.target.value })} />
-			<input type="email" name="email" placeholder="Email" onChange={(e) => setGuide({ ...guide, email: e.target.value })} />
-			<input type="password" name="password" placeholder="Password" onChange={(e) => setGuide({ ...guide, password: e.target.value })} />
-			<input type="text" name="domain" placeholder="Domain" onChange={(e) => setGuide({ ...guide, domain: e.target.value })} />
-			<button onClick={addGuide}>Add Guide</button>
-			<ol>
-				{guides.length !== 0 ? (
-					guides.map((ele) => {
-						return <li key={ele._id}>{ele.name}</li>;
-					})
-				) : (
-					<p>no Guides</p>
-				)}
-			</ol>
+		// <div className="min-vh-100 p-4 bg-dark text-danger">
+		// 	Coordinator dashboard
+		// 	<button onClick={logout}>Logout</button>
+		// 	<br />
+		// 	{/* add batch */}
+		// 	<input type="year" placeholder="Year" onChange={(e) => setBatch({ ...batch, year: e.target.value })} value={batch.year} />
+		// 	<input type="text" placeholder="Branch" onChange={(e) => setBatch({ ...batch, branch: e.target.value })} value={batch.branch} />
+		// 	<input type="text" name="domains" placeholder="Domains (comma-separated)" onChange={handleDomainsetChange} value={batch.domainset.join(", ")} />
+		// 	<button onClick={createBatch}>Create Batch</button>
+		// 	<ul>
+		// 		{batches.length !== 0 ? (
+		// 			batches.map((ele) => {
+		// 				return (
+		// 					<li key={ele._id}>
+		// 						year: {ele.year}, branch: {ele.branch}, domainset: {ele.domainset ? ele.domainset.join(", ") : "No domains"}
+		// 						<button onClick={() => router.push("dashboard/" + ele._id)}>GOTO</button>
+		// 					</li>
+		// 				);
+		// 			})
+		// 		) : (
+		// 			<p>no batches</p>
+		// 		)}
+		// 	</ul>
+		// 	{/* add guide */}
+		// 	<br />
+		// 	<input type="text" name="name" placeholder="Name" onChange={(e) => setGuide({ ...guide, name: e.target.value })} />
+		// 	<input type="email" name="email" placeholder="Email" onChange={(e) => setGuide({ ...guide, email: e.target.value })} />
+		// 	<input type="password" name="password" placeholder="Password" onChange={(e) => setGuide({ ...guide, password: e.target.value })} />
+		// 	<input type="text" name="domain" placeholder="Domain" onChange={(e) => setGuide({ ...guide, domain: e.target.value })} />
+		// 	<button onClick={addGuide}>Add Guide</button>
+		// 	<ol>
+		// 		{guides.length !== 0 ? (
+		// 			guides.map((ele) => {
+		// 				return <li key={ele._id}>{ele.name}</li>;
+		// 			})
+		// 		) : (
+		// 			<p>no Guides</p>
+		// 		)}
+		// 	</ol>
+		// </div>
+		<div>
+			<div style={main}>
+				<div style={sidebar}>
+					<div className="d-flex">
+						<PgCoordinatorSidebar />
+					</div>
+				</div>
+				{/* <div style={content}>
+				<AddGuides />
+				<Outlet />
+			</div> */}
+				<div style={content}>
+					<div
+						className="d-flex justify-content-between"
+						style={{
+							marginTop: "10px",
+						}}>
+						<p>Computer Science 2023-24</p>
+					</div>
+					<button onClick={logout}>Logout</button>
+					{/* <GuideSubmissionView /> */}
+					{/* <GuideApproval /> */}
+					{/* <Outlet /> */}
+				</div>
+				<div style={rightbar}>
+					<Rightbar />
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -36,26 +36,51 @@ export default function Page() {
 	const onchange = (e) => {
 		setInfo({ ...info, [e.target.name]: e.target.value });
 	};
+
+	//styles =>
+	const inputStyle = { width: "50dvw", height: "40%", padding: "10px", borderRadius: "10px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", border: "none", color: "#004257", fontSize: "16px", fontWeight: "600", fontFamily: "Roboto, sans-serif", marginBottom: "20px" };
+
+	const headerStyle = { fontSize: "32px", fontWeight: "700", marginBottom: "5px" };
+
+	const cardStyle = { border: "none", padding: "20px", borderRadius: "5px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", color: "#004257", background: "#E1F8FF", fontFamily: "Roboto, sans-serif", textAlign: "center" };
+
+	const buttonstyle = {
+		width: "176px",
+		height: "50px",
+		padding: "10px",
+		backgroundColor: "#004257",
+		borderRadius: "10px",
+		border: "none",
+		color: "#fff",
+		fontSize: "24px",
+		fontWeight: "600",
+		boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 4px rgba(0, 0, 0, 0.25)",
+		marginBottom: "20px",
+	};
+
 	return (
-		<div className="min-vh-100 p-4">
-			<div className="card border p-4 w-50 min-vh-500 container align-self-center overflow-hidden">
-				<h1 className="text-center">Login</h1>
+		<div className="login-box" style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
+			<div className="card border" style={cardStyle}>
+				{/* <h1 className="text-center">Login</h1> */}
+				<p style={headerStyle}>ResearchHouse</p>
+				<p style={headerStyle}>Login</p>
 
 				<form onSubmit={onSubmit} className="">
 					<div className="mb-3">
-						<label htmlFor="email" className="form-label">
+						{/* <label htmlFor="email" className="form-label">
 							Email
-						</label>
-						<input id="email" className="form-control" required name="email" value={info.email} onChange={onchange} />
+						</label> */}
+						<input placeholder="Email" id="email" name="email" required className="form-control" style={inputStyle} value={info.email} onChange={onchange} />
+						{/* <input id="email" className="form-control" required name="email" value={info.email} onChange={onchange} /> */}
 					</div>
 					<div className="mb-3">
-						<label htmlFor="password1" className="form-label">
+						{/* <label htmlFor="password1" className="form-label">
 							Password:
-						</label>
-						<input id="password1" type="password" className="form-control" minLength={3} maxLength={8} required name="password" value={info.password} onChange={onchange} />
+						</label> */}
+						<input placeholder="Password" id="password1" type="password" className="form-control" minLength={3} maxLength={8} required name="password" value={info.password} onChange={onchange} style={inputStyle} />
 					</div>
 					<div className="w-100 text-center">
-						<button type="submit" className="btn btn-secondary">
+						<button type="submit" className="submitButton" style={buttonstyle}>
 							Login
 						</button>
 					</div>
