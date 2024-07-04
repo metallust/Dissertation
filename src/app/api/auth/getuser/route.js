d4adec5ebf052835d70a9eb070927fe18dd24c17
Basit-Perampalli <apeerampalli@gmail.com>
2024-07-05 01:39:15 +0530

    Post method in route.js

--- src/app/api/auth/getuser/route.js
+++ src/app/api/auth/getuser/route.js
@@ -30,3 +30,29 @@ export async function GET(request) {
 		return NextResponse.json({ error: error.message }, { status: 400 });
 	}
 }
+
+
+export async function POST(request) {
+	try {
+		const token = request.cookies.get("token")?.value;
+		const { id } = await request.json();
+		if (!token) {
+			return NextResponse.redirect(new URL("/login", request.nextUrl));
+		}
+		const tokenData = await getDataFromToken(token);
+		const user = await User.findOne({ _id: id }).select(
+			"-password",
+		);
+		if (!user) {
+			return NextResponse.json(
+				new Response(404, "User not found", null),
+				{ status: 404 },
+			);
+		}
+		return NextResponse.json(new Response(200, "User found", user), {
+			status: 200,
+		});
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 400 });
+	}
+}
